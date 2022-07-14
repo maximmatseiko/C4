aa540a8bf0aa2b019002c427cf3dfb3533068c31
max <maczejko.max@ya.ru>
2022-07-14 18:27:33 +0600

    задача 1

---
+++ les01/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+System.Console.WriteLine("Введите трёхзначное число :");
+int NumA=Convert.ToInt32(Console.ReadLine());
+string Num=Convert.ToString(NumA);
+System.Console.WriteLine("Вторая цыфра :"+ Num[2]);
