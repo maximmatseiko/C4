4add803b0444c27dbb176ca2ade9f4f9786bc80d
max <maczejko.max@ya.ru>
2022-07-14 18:55:39 +0600

    задача 2

---
+++ les02/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+        System.Console.WriteLine("Введите числа :");
+        int NumA = Convert.ToInt32(Console.ReadLine());
+        string Num = Convert.ToString(NumA);
+        if (Num.Length>2)
+        {
+            System.Console.WriteLine("Третья цыфра :"+Num[2]);
+        }
+        else 
+        {
+            System.Console.WriteLine("Третьей цыфры нет");
+        }
+    }   
+}
